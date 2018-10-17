825ce79122b8242c97be687d851d58687dc2630c
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-10-17 08:24:38 +0300

    Updates

---
+++ precache-manifest.27b7133e2114ca9221189544647a0083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weatherai/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ca55e6a9ba2ba7eb44af",
+    "url": "/weatherai/static/js/runtime~main.ca55e6a9.js"
+  },
+  {
+    "revision": "e48583111e9f8459d8c8",
+    "url": "/weatherai/static/js/main.e4858311.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/weatherai/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e48583111e9f8459d8c8",
+    "url": "/weatherai/static/css/main.59e84383.chunk.css"
+  },
+  {
+    "revision": "8636136680617ac0994b3c62caaeada2",
+    "url": "/weatherai/index.html"
+  }
+];
